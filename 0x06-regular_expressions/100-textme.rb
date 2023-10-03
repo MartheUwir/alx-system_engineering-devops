4ab28d6371b1dfca6b9930f2367cbe8cf9e1adb0
MartheUwir <marthuwir@gmail.com>
2023-10-03 17:59:26 +0200

    logo

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
-ptn = /(?:(?<=from:)(?:\p{L}+|\+?\d+)|(?<=to:)(?:\p{L}+|\+?\d*)|(?<=flags:)(?:-?\d+:?)*)/
-puts ARGV[0].scan(ptn).join(',')
+# Match Sender, Reciever and flags from 
+# Message logs
+
+puts ARGV[0].scan(/from:+\K(\d{11})/).join
